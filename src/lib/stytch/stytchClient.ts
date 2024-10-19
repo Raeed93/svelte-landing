bd309b10383a0340b3857fb66447d347041e12af
Breezyoss <raed@remedium-network.io>
2024-10-19 14:13:51 +0300

    Test

--- src/lib/stytch/stytchClient.ts
+++ src/lib/stytch/stytchClient.ts
@@ -5,7 +5,7 @@
 import * as stytch from 'stytch';
 
 // Initialize the Stytch client using environment variables
-
+//Test
 // Check that environment variables are defined
 const project_id = process.env.STYTCH_PROJECT_ID;
 const secret = process.env.STYTCH_SECRET;
